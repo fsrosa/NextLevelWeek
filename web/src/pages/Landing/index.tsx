67df4e9222a820c065be29c4733ad08d72e97d4b
Fabrício Rosa <fabriciosilvarosa@gmail.com>
2020-11-06 18:05:45 -0300

    testing

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -4,12 +4,14 @@ import logoImg from '../../assets/images/logo.svg'
 import landingImg from '../../assets/images/landing.svg'
 
 function Landing() {
+
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
                 <div className="logo-container">
                     <img src={logoImg} alt="Proffy" />
                     <h2>Sua plataforma de estudos online.</h2>
+                    <h3>Fred que mala</h3>
                 </div>
 
                 <img

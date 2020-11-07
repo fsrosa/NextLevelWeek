86c69e4634a7a76a99e24eff14cf0cae5e61bc8e
Fabrício Rosa <fabriciosilvarosa@gmail.com>
2020-11-06 22:38:09 -0300

    implementing landingpage with mobile first

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -3,6 +3,13 @@ import React from 'react'
 import logoImg from '../../assets/images/logo.svg'
 import landingImg from '../../assets/images/landing.svg'
 
+import studyIcon from '../../assets/images/icons/study.svg'
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
+
+import './styles.css'
+
+
 function Landing() {
 
     return (
@@ -20,10 +27,19 @@ function Landing() {
                 />
 
                 <div className="buttons-container">
-                    <a href="" className="">
-
+                    <a href="" className="study">
+                        <img src={studyIcon} alt="Estudar"/>
+                        Estudar
+                    </a>
+                    <a href="" className="give-classes">
+                        <img src={giveClassesIcon} alt="Estudar"/>
+                        Estudar
                     </a>
                 </div>
+
+                <span className="total-connections">
+                    Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
+                </span>
             </div>
         </div>
     )
